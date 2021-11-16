781f355da41015d39df23d5576eed98450ffdcdc
faozziyyah <omowunmidaud1@gmail.com>
2021-11-16 05:55:58 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Amenities from "./components/Amenities";
+import Testimonial from "./components/Testimonial";
+import Membership from "./components/Membership";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <About />
+      <Amenities />
+      <Testimonial />
+      <Membership />
+      <Footer />
     </div>
   );
 }
